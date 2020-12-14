64ec897274e992331dc76b274c0f3d9738489b83
cchura94 <cchura.cpc@gmail.com>
2020-12-13 20:51:26 -0400

    CORS

--- src/index.js
+++ src/index.js
@@ -30,6 +30,14 @@ var app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // habilitar rutas
 rutas.adicionar(app);
 
